eaacf2ff94e1faf5e31f2a1e2417952a3678cbff
ProfesorAlbino <JEYKEL.MEDRANO@ucr.ac.cr>
2024-07-22 16:25:26 -0600

    Creacion de componente CardImageText

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <script setup>
 import wave from '../components/Wave.vue'
-import CardImage from '../components/CardImage.vue'
+import CardImageText from '../components/CardImageText.vue'
 </script>
 
 <template>
-  <wave class="mb-60" url="../assets/Fondo-pagina-principal.jpg" title="Pagina principal APAZA" description="Prueba" />
-  <CardImage />
-  <CardImage />
-  <CardImage />
-  <CardImage />
-  <CardImage />
-  <CardImage />
-  <CardImage />
-  <CardImage />
-  <CardImage />
+  <wave class="mb-20" url="../assets/Fondo-pagina-principal.jpg" title="Pagina principal APAZA" description="Prueba" />
+
+  <CardImageText title="¿Que es el autismo?" description="El autismo es un trastorno del desarrollo que afecta la forma en que una persona se relaciona con el
+                mundo y con
+                las personas que lo rodean. Las personas con autismo pueden tener dificultades para comunicarse y
+                relacionarse
+                con los demás, y pueden tener intereses y comportamientos repetitivos."
+    url="src/assets/Fondo-pagina-principal.jpg" :order="true" />
+
+  <CardImageText title="¿Que es el autismo?" description="El autismo es un trastorno del desarrollo que afecta la forma en que una persona se relaciona con el
+                mundo y con
+                las personas que lo rodean. Las personas con autismo pueden tener dificultades para comunicarse y
+                relacionarse
+                con los demás, y pueden tener intereses y comportamientos repetitivos."
+    url="src/assets/Fondo-pagina-principal.jpg" :order="false" />
+
 </template>
