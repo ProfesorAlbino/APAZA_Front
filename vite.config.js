72a74a764e4f276f75adadbee5f67136929a2a9b
ProfesorAlbino <JEYKEL.MEDRANO@ucr.ac.cr>
2024-09-10 19:11:25 -0600

    Configuracion SSL

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
+   import { defineConfig } from 'vite'
+   import vue from '@vitejs/plugin-vue'
+   import basicSsl from '@vitejs/plugin-basic-ssl'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+   // https://vitejs.dev/config/
+   export default defineConfig({
+     plugins: [
+       vue(),
+       basicSsl()
+     ],
+     resolve: {
+       alias: {
+         '@': fileURLToPath(new URL('./src', import.meta.url))
+       }
+     }
+   })
