21330b48e32d08193da2a1affd7ec681e01bd551
ProfesorAlbino <JEYKEL.MEDRANO@ucr.ac.cr>
2024-09-21 14:25:06 -0600

    Cambios

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
    import vue from '@vitejs/plugin-vue'
    import basicSsl from '@vitejs/plugin-basic-ssl'
 
-   // https://vitejs.dev/config/
    export default defineConfig({
      plugins: [
        vue(),
