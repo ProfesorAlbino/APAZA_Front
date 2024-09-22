790d00352d54e6c7626100bbb968ef3d3696b89a
Elias <jmenafonseca28@gmail.com>
2024-09-21 21:45:15 -0600

    Error que jona me hizo poner

--- src/components/toasts/NotificationToast.vue
+++ src/components/toasts/NotificationToast.vue
@@ -16,7 +16,6 @@ defineProps({
     <div class="toast-container position-fixed top-0 end-0 p-3">
         <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
-                <img src="..." class="rounded me-2" alt="...">
                 <strong class="me-auto">{{ header }}</strong>
                 <!-- <small>11 mins ago</small> -->
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
