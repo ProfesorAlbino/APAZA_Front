71241e4e48ef11bfc45d2b2ddf5b6adf3c2359d5
ProfesorAlbino <JEYKEL.MEDRANO@ucr.ac.cr>
2024-09-24 16:44:36 -0600

    Cambio de rutas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,7 +29,7 @@ onMounted(() => {
 
 <template>
   <section>
-    <wave class="mb-20" url="/public/Apaza/APAZA_FONDO.jpg" title="" description="" />
+    <wave class="mb-20" url="/Apaza/APAZA_FONDO.jpg" title="" description="" />
   </section>
 
   <section>
@@ -58,19 +58,19 @@ onMounted(() => {
 
     <div class="row mt-5">
       <div class="col-4">
-        <CardFullImage title="Recién Diagnosticado" url="/public/Apaza/Recien Diagnosticado.jpg"
+        <CardFullImage title="Recién Diagnosticado" url="/Apaza/Recien Diagnosticado.jpg"
           descriptionCard="Esta sección ofrece información y apoyo para personas que han recibido recientemente un diagnóstico de autismo"
           link="#" data-aos="zoom-out-up"/>
       </div>
 
       <div class="col-4">
-        <CardFullImage title="Síntomas del Autismo" url="/public/Apaza/Sintomas del Autismo.jpg"
+        <CardFullImage title="Síntomas del Autismo" url="/Apaza/Sintomas del Autismo.jpg"
           descriptionCard="Conoce los síntomas y señales comunes del autismo para una identificación temprana y precisa."
           link="#" data-aos="zoom-out-up"/>
       </div>
 
       <div class="col-4">
-        <CardFullImage title="Tratamientos" url="/public/Apaza/Tratamientos.jpg"
+        <CardFullImage title="Tratamientos" url="/Apaza/Tratamientos.jpg"
           descriptionCard="Explora diferentes tratamientos y terapias que pueden ayudar en el desarrollo y bienestar de personas con autismo."
           link="#" data-aos="zoom-out-up"/>
       </div>
@@ -89,19 +89,19 @@ onMounted(() => {
       </div>-->
 
       <div class="col-4">
-        <CardFullImage title="Sobre Nosotros" url="/public/Apaza/Fondo-pagina-principal.jpg"
+        <CardFullImage title="Sobre Nosotros" url="/Apaza/Fondo-pagina-principal.jpg"
           descriptionCard="Esta sección ofrece información y apoyo para personas que han recibido recientemente un diagnóstico de autismo"
           link="/about" data-aos="zoom-out-up"/>
       </div>
 
       <div class="col-4">
-        <CardFullImage title="Nuestros Eventos" url="/public/Apaza/Nuestros Eventos.jpg"
+        <CardFullImage title="Nuestros Eventos" url="/Apaza/Nuestros Eventos.jpg"
           descriptionCard="Explora nuestros eventos y enterate de todas nuestras actividades para no perderte ninguna."
           link="#" data-aos="zoom-out-up"/>
       </div>
 
       <div class="col-4">
-        <CardFullImage title="Eventos Comunitarios" url="/public/Apaza/Eventos Comunitarios.jpg"
+        <CardFullImage title="Eventos Comunitarios" url="/Apaza/Eventos Comunitarios.jpg"
           descriptionCard="Asiste a eventos organizados para conectar a familias y personas dentro del espectro autista."
           link="#" data-aos="zoom-out-up"/>
       </div>
