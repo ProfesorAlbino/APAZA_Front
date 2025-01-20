de1257fd9873fadf9aeeb6de03d88bd7f1c352eb
ProfesorAlbino <JEYKEL.MEDRANO@ucr.ac.cr>
2025-01-20 11:38:02 -0600

    Cambios en homepage y aboutUs para vista en moviles

--- src/components/Wave.vue
+++ src/components/Wave.vue
@@ -19,8 +19,13 @@ const props = defineProps({
 
 <template>
     <section
-        class="vh-100 d-flex align-items-center justify-content-center text-center text-white position-relative overflow-hidden"
-        :style="{ backgroundSize: '100%', backgroundImage: `url(${props.url})`, backgroundPosition: 'center' }">
+    class="vh-100 d-flex align-items-center justify-content-center text-center text-white position-relative overflow-hidden"
+    :style="{ 
+        backgroundSize: 'cover', 
+        backgroundRepeat: 'no-repeat', 
+        backgroundImage: `url(${props.url})`, 
+        backgroundPosition: 'center' 
+    }">
         <div>
             <div class="row justify-content-center">
                 <div class="col-md-8">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
 import wave from '@/components/Wave.vue';
 import CardImageRight from '../components/cards/CardImageRight.vue';
 import CardImageLeft from '../components/cards/CardImageLeft.vue';
-import CardFullImage from '../components/cards/CardFullImage.vue';
-import SectionInfo from '../components/templates/SectionInfo.vue'
+import CardFullImage from '@/components/cards/CardFullImage.vue';
+
 import { getLangForPage, getConfig } from '@/config/BasicConfig';
 import { ref, onMounted } from 'vue';
 import { initPreloader, removePreloader } from '@/components/loaders/useBaseLoader'
@@ -27,6 +27,11 @@ onMounted(async () => {
   });
 });
 
+function navigate(url) {
+    router.push(url);
+}
+
+
 
 </script>
 
@@ -36,7 +41,7 @@ onMounted(async () => {
       data-aos-duration="1000" />
   </section>
 
-  <section>
+  <section id="principal">
     <CardImageRight :title="lang.homepage?.titles?.whatIsTea || ''"
       :description="lang.homepage?.body?.descriptionTea || ''" url="/Apaza/WhatIsTheAutism.png" :order="false"
       data-aos="zoom-in-right" />
@@ -46,178 +51,169 @@ onMounted(async () => {
       :description="lang.homepage?.body?.descriptionCharacteristicsTea || ''" url="/Apaza/FirstCharacteristics.png"
       :order="false" data-aos="zoom-in-left" />
   </section>
-  
-  <section>
-    <SectionInfo :title="lang.homepage?.titles?.titleAboutUs || ''" 
-    :subtitle="lang.homepage?.body?.descriptionAboutUs || ''"
-    url="/Test/about-left-image.png"
-    :count1="100" :count2="2011" :count3="30"
-    :countTitle1="lang.homepage?.titles?.countTitle1 || ''" 
-    :countTitle2="lang.homepage?.titles?.countTitle2 || ''" 
-    :countTitle3="lang.homepage?.titles?.countTitle3 || ''"
-    :countDescription1="lang.homepage?.body?.countDescription1 || ''"
-    :countDescription2="lang.homepage?.body?.countDescription2 || ''"
-    :countDescription3="lang.homepage?.body?.countDescription3 || ''"
-    link="/about"
-    data-aos="zoom-in-left"/>
-  </section>
 
-  <section id="sectionCards" class="a">
-
-    <div class="custom-shape-divider-top-1721696071">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path
-          d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-          class="shape-fill"></path>
-      </svg>
-    </div>
-
-
-    <h1 id="title" class="text-center" data-aos="fade-up" data-aos-duration="3000">{{
-      lang.homepage?.titles?.informationTeaApaza || '' }}</h1>
-
-
-    <div class="row mt-5">
-
-      <div class="col-4">
-        <CardFullImage :title="lang?.titleAboutUs || ''" url="/Apaza/Fondo-pagina-principal.jpg"
-          :descriptionCard="lang.homepage?.body?.infoAboutUs || ''" link="/about" data-aos="zoom-out-up" />
+  <section class="about-section">
+    <div class="row align-items-center">
+      <div class="col-lg-6 mb-4 mb-lg-0">
+        <img src="/Apaza/APAZA_FONDO.jpg" alt="Acerca de Nosotros" class="img-fluid about-image">
       </div>
+      <div class="col-lg-6">
+        <h2 class="about-title">Acerca de Nosotros</h2>
+        <div class="about-text">
+          <p>Somos una empresa dedicada a la <span class="highlight-text">excelencia e innovación</span>,
+            comprometida con brindar soluciones de alta calidad a nuestros clientes desde hace más de una década.</p>
 
-      <div class="col-4">
-        <CardFullImage :title="lang.homepage?.titles?.characteristicsTea || ''" url="/Apaza/CharacteristicsOfAutism.jpg"
-          :descriptionCard="lang.homepage?.body?.infoCharacteristicsTea || ''" link="#" data-aos="zoom-out-up" />
+          <p>Nuestro equipo de profesionales altamente capacitados trabaja incansablemente para
+            garantizar que cada proyecto cumpla con los más altos estándares de calidad y satisfacción del cliente.</p>
+
+          <p>Con una visión clara hacia el futuro y un compromiso inquebrantable con la
+            <span class="highlight-text">mejora continua</span>, seguimos expandiendo nuestros horizontes y
+            adaptándonos a las necesidades cambiantes del mercado.
+          </p>
+          <a @click="navigate('/about')" class="btn_about btn btn-primary" >¡Conócenos!</a>
+        </div>
       </div>
-
-      <div class="col-4">
-        <CardFullImage :title="lang.homepage?.titles?.treatmentsTitle || ''" url="/Apaza/Tratamientos.jpg"
-          :descriptionCard="lang.homepage?.body?.infoTreatments || ''" link="#" data-aos="zoom-out-up" />
-      </div>
-
-    </div>
-
-    <h1 id="title" class="text-center mt-5">{{ lang.services + ' - ' + lang.events }}</h1>
-
-    <div class="row mt-5">
-      <div class="col-4">
-        <CardFullImage :title="lang.homepage?.titles?.recentDiagnosis || ''" url="/Apaza/FirstDiagnostics.jpg"
-          :descriptionCard="lang.homepage?.body?.descriptionHelpDiagnostic || ''" link="#" data-aos="zoom-out-up" />
-      </div>
-
-      <div class="col-4">
-        <CardFullImage :title="lang?.events || ''" url="/Apaza/NewEvents.jpg"
-          :descriptionCard="lang.homepage?.body?.infoEvents || ''" link="#" data-aos="zoom-out-up" />
-      </div>
-
-      <div class="col-4">
-        <CardFullImage :title="lang?.events || ''" url="/Apaza/CommunityEvents.jpg"
-          :descriptionCard="lang.homepage?.body?.infoEvents || ''" link="#" data-aos="zoom-out-up" />
-      </div>
-    </div>
-
-    <div class="custom-shape-divider-bottom-1721776550">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path
-          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-          class="shape-fill"></path>
-      </svg>
+      
     </div>
   </section>
+
+  <svg id="wave" style="transform:rotate(0deg); transition: 0.3s" viewBox="0 0 1440 170" version="1.1"
+    xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+        <stop stop-color="var(--primary-color)" offset="0%"></stop>
+        <stop stop-color="var(--primary-color)" offset="100%"></stop>
+      </linearGradient>
+    </defs>
+    <path style="transform:translate(0, 0px); opacity:1" fill="url(#sw-gradient-0)"
+      d="M0,85L60,96.3C120,108,240,130,360,119C480,108,600,62,720,59.5C840,57,960,96,1080,99.2C1200,102,1320,68,1440,45.3C1560,23,1680,11,1800,19.8C1920,28,2040,57,2160,62.3C2280,68,2400,51,2520,45.3C2640,40,2760,45,2880,48.2C3000,51,3120,51,3240,42.5C3360,34,3480,17,3600,17C3720,17,3840,34,3960,39.7C4080,45,4200,40,4320,51C4440,62,4560,91,4680,87.8C4800,85,4920,51,5040,42.5C5160,34,5280,51,5400,68C5520,85,5640,102,5760,102C5880,102,6000,85,6120,82.2C6240,79,6360,91,6480,96.3C6600,102,6720,102,6840,99.2C6960,96,7080,91,7200,73.7C7320,57,7440,28,7560,22.7C7680,17,7800,34,7920,39.7C8040,45,8160,40,8280,42.5C8400,45,8520,57,8580,62.3L8640,68L8640,170L8580,170C8520,170,8400,170,8280,170C8160,170,8040,170,7920,170C7800,170,7680,170,7560,170C7440,170,7320,170,7200,170C7080,170,6960,170,6840,170C6720,170,6600,170,6480,170C6360,170,6240,170,6120,170C6000,170,5880,170,5760,170C5640,170,5520,170,5400,170C5280,170,5160,170,5040,170C4920,170,4800,170,4680,170C4560,170,4440,170,4320,170C4200,170,4080,170,3960,170C3840,170,3720,170,3600,170C3480,170,3360,170,3240,170C3120,170,3000,170,2880,170C2760,170,2640,170,2520,170C2400,170,2280,170,2160,170C2040,170,1920,170,1800,170C1680,170,1560,170,1440,170C1320,170,1200,170,1080,170C960,170,840,170,720,170C600,170,480,170,360,170C240,170,120,170,60,170L0,170Z">
+    </path>
+  </svg>
+  <section id="sectionCards" class="py-4">
+    <h1 class="title text-center mb-5">Nuestros Servicios</h1>
+
+    <div class="row g-4">
+      <div class="col-12 col-md-4">
+        <CardFullImage :title="lang.homepage?.titles?.events || ''"
+          :descriptionCard="lang.homepage?.body?.descriptionEvents || ''" url="/Apaza/Events.png" link="/events"
+          data-aos="zoom-in" />
+      </div>
+      <div class="col-12 col-md-4">
+        <CardFullImage :title="lang.homepage?.titles?.events || ''"
+          :descriptionCard="lang.homepage?.body?.descriptionEvents || ''" url="/Apaza/Events.png" link="/events"
+          data-aos="zoom-in" />
+      </div>
+      <div class="col-12 col-md-4">
+        <CardFullImage :title="lang.homepage?.titles?.events || ''"
+          :descriptionCard="lang.homepage?.body?.descriptionEvents || ''" url="/Apaza/Events.png" link="/events"
+          data-aos="zoom-in" />
+      </div>
+    </div>
+
+    <h1 class="title text-center mt-5 mb-5">Nuestros Eventos</h1>
+
+    <div class="row g-4">
+      <div class="col-12 col-md-4">
+        <CardFullImage :title="lang.homepage?.titles?.events || ''"
+          :descriptionCard="lang.homepage?.body?.descriptionEvents || ''" url="/Apaza/Events.png" link="/events"
+          data-aos="zoom-in" />
+      </div>
+      <div class="col-12 col-md-4">
+        <CardFullImage :title="lang.homepage?.titles?.events || ''"
+          :descriptionCard="lang.homepage?.body?.descriptionEvents || ''" url="/Apaza/Events.png" link="/events"
+          data-aos="zoom-in" />
+      </div>
+      <div class="col-12 col-md-4">
+        <CardFullImage :title="lang.homepage?.titles?.events || ''"
+          :descriptionCard="lang.homepage?.body?.descriptionEvents || ''" url="/Apaza/Events.png" link="/events"
+          data-aos="zoom-in" />
+      </div>
+    </div>
+
+  </section>
+
   <BaseLoader />
 </template>
 
 <style scoped>
 #sectionCards {
+  margin-top: -5px;
   background-color: var(--primary-color);
-  display: flex;
-
-  padding: 10rem;
-  position: relative;
-  height: 200%;
-  color: var(--text-color-1);
-  font-family: var(--text-font-1);
-
-  display: flex;
-  flex-direction: column;
 }
 
-#title {
+.title {
   color: var(--text-color-1);
   font-family: var(--text-font-1);
 }
 
-.custom-shape-divider-top-1721696071 {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  overflow: hidden;
-  line-height: 0;
-  transform: rotate(180deg);
+.about-section {
+  background-color: var(--background-color-3);
+  padding: 5rem 0;
+  font-family: var(--text-font-1);
 }
 
-.custom-shape-divider-top-1721696071 svg {
+.about-title {
+  color: var(--text-color-2);
+  font-weight: 700;
+  margin-bottom: 1.5rem;
   position: relative;
-  display: block;
-  width: calc(100% + 1.3px);
-  height: 84px;
 }
 
-.custom-shape-divider-top-1721696071 .shape-fill {
-  fill: var(--background-color-3);
-}
-
-/*
-.custom-shape-divider-top-1721696071 {
+.about-title::after {
+  content: '';
   position: absolute;
-  top: 0;
+  bottom: -10px;
   left: 0;
-  width: 100%;
-  overflow: hidden;
-  line-height: 0;
-  transform: rotate(180deg);
+  width: 80px;
+  height: 3px;
+  background-color: var(--background-color-2);
 }
 
-.custom-shape-divider-top-1721696071 svg {
-  position: relative;
-  display: block;
-  width: calc(300% + 1.3px);
-  height: 84px;
+.about-text {
+  color: var(--text-color-3);
+  font-size: 1.1rem;
+  line-height: 1.8;
 }
 
-.custom-shape-divider-top-1721696071 .shape-fill {
-  fill: var(--background-color-3);
-  animation: wave-animation 15s linear infinite;
+.about-image {
+  border-radius: 10px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease;
 }
 
-@keyframes wave-animation {
-  0% {
-    transform: translateX(0%);
+.about-image:hover {
+  transform: scale(1.02);
+}
+
+.highlight-text {
+  color: var(--background-color-2);
+  font-weight: 600;
+}
+
+@media (max-width: 768px) {
+  .about-section {
+    padding: 3rem 0;
   }
 
-  100% {
-    transform: translateX(-33.33%);
+  .about-image {
+    margin-bottom: 2rem;
   }
-} */
-
-.custom-shape-divider-bottom-1721776550 {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  overflow: hidden;
-  line-height: 0;
-  transform: rotate(180deg);
 }
 
-.custom-shape-divider-bottom-1721776550 svg {
-  position: relative;
-  display: block;
-  width: calc(100% + 1.3px);
-  height: 131px;
+.btn_about{
+  background-color: var(--background-color-2);
+  color: var(--text-color-1);
+  font-family: var(--text-font-1);
+  font-weight: 600;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
 }
 
-.custom-shape-divider-bottom-1721776550 .shape-fill {
-  fill: #FFFFFF;
+.btn_about:hover{
+  background-color: var(--background-color);
+
 }
+
 </style>
